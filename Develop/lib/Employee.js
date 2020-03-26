46f87ccaaf5987efb477d24553b124aa252c25cc
imAnonAmi <mcintireaw@gmail.com>
2020-03-26 16:34:52 -0700

    Sweet Lord. First suite of tests all pass.

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,27 @@
-// TODO: Write code to define and export the Employee class
+
+class Employee {
+    constructor(name, id, email) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+    }
+    //return name
+    getName(){
+        return this.name;
+    };
+
+    //return id
+    getId(){
+        return this.id;
+    };
+    //return email
+    getEmail(){
+        return this.email;
+    };
+    //return role
+    getRole() {
+        return "Employee";
+    };
+  };
+
+  module.exports = Employee;
