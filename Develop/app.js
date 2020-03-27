ec04c22dda811a2aafb459c4dbc9b852d43e7b92
imAnonAmi <mcintireaw@gmail.com>
2020-03-27 16:12:28 -0700

    Changed inquirer method, but still not working

--- Develop/app.js
+++ Develop/app.js
@@ -4,64 +4,87 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 //This is where we make a variable that is the relative path of our output directory. Read up more on path: https://nodejs.org/docs/latest/api/path.html later
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
+const writeFileAsync = util.promisify(fs.writeFile);
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-function buildTeam(){
+function buildTeam() {
+    console.log("Welcome to Team Template Engine.");
     return inquirer.prompt([
     {
         type: "input",
         name: "name",
-        message: "Hello team builder! To begin, what is the name of your Manager?:"
+        message: "Hello team builder! To begin, what is the name of your Manager?:",
+        default: function() {
+            return "Tom";
+        }
     },
 //REMEMBER: commas not semi-colons
     {
         type: "input",
         name: "id",
-        message: "Great! What is your Manager's ID?:"
+        message: "Great! What is your Manager's ID?:",
+        default: function() {
+            return "2";
+        }
     },
     {
         type: "input",
         name: "email",
-        message: "What is your Manager's email address?:"
+        message: "What is your Manager's email address?:",
+        default: function() {
+            return "Tom@tom.com";
+        }
     },
     {
         type: "input",
         name: "officeNumber",
-        message: "What is your Manager's office number?:"
+        message: "What is your Manager's office number?:",
+        default: function() {
+            return "22";
+        }
     },
-
-
-
 ]);
 }
-// After the user has input all employees desired, call the `render` function (required
+// After the user has input all answers desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+function render()
 
- async function init() {
-// Make sure it works, states purpose
-     console.log("Welcome to Team Template Engine.");
-     
-     try {
-        const employees = await buildTeam();
-//Pass information into render
-        render(employees);
-//Use fs to create the HTML roster
-        await render("team.html", outputPath);
+ buildTeam() 
+    .then(function(answers) {
+        const html = render(answers);
 
-        console.log("Your team template has been built!")
-     } catch (err)
-    {
+        return writeFileAsync(outputPath, html);
+    })
+    .then(function() {
+        console.log("Your team template has been built!");
+    })
+    .catch(function(err){
         console.log(err);
-    }
- };
+    });
+// Make sure it works, states purpose
+     
+//Commenting out below for now and trying to use the method from Mini Project     
+//     try {
+//        const answers = await buildTeam();
+//Pass information into render
+//        render(answers);
+//Use fs to create the HTML roster
+//        await render("team.html", outputPath);
+
+//        console.log("Your team template has been built!")
+//     } catch (err)
+//    {
+//      console.log(err);
+//    }
+// };
  
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
